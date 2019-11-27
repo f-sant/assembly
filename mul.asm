b53e95f7c05d017ef7b166e94346b5f4e5d962ee
Filipe Santos <fsant@protonmail.com>
2019-11-27 17:15:27 -0300

    implement mul as a subroutine

--- mul.asm
+++ mul.asm
@@ -3,21 +3,24 @@ org 1000h
 num1    db  5h
 num2    db  3h
 
-;instructions
+;subroutine
+org 3000h
+mul: cmp al, 0 ;compare 0 with the al value
+    jz end ;go to the label end if it's equal
+    cmp cl, 0 ;compare 0 with the cl value
+    jz end ; go to the label end if it's equal
+    mov ah, 0 ;move 0 to the reg ah
+    mov dx, 0 ;move 0 to the reg dx
+loop: add dx, ax ; add num2 times
+      dec cl ; dec cl, the previous op. will be made (CL) x times
+      jnz loop ;go to label loop if cl that contains num2 value is dif 0 (repeat)
+end: ret
 org 2000h
 mov al, num1 ;move num1 to reg al
-cmp al, 0 ;compare 0 with the al value
-jz  end1 ;go to the label end if it's equal
-mov ah, 0 ;will be used for operation
-mov dx, 0 ;res will be stored here
 mov cl, num2 ; move num2 to reg cl
-loop: cmp cl, 0 ;compare 0 with the cl value
-    jz end ;go to the label end if it's equal
-    add dx, ax ;add dx (res) with ax (num1) res=res+num1 
-    dec cl ; dec cl, the previous op. will be made (CL) x times
-    jump loop ;go to label loop (repeat)
-end1:hlt
-    end
+call mul
+hlt
+end
 
 
 
